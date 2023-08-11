4942f23e91c477795e8072405cf4a69217af6317
NguyenHoangVu12 <116646306+NguyenHoangVu12@users.noreply.github.com>
2023-08-11 19:01:30 +0700

    abc

--- BTTHIETKEWEB/file.js
+++ BTTHIETKEWEB/file.js
@@ -1,6 +1,41 @@
+//Đăng kí
+function signup(e){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var user = {
+        username : username,
+        email : email,
+        password : password,
+    }
+    var json = JSON.stringify(user);
+    localStorage.setItem(username,json);
+    alert("Đăng ký thành công")
+}
+
+//Đăng nhập
+function login(e){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var user = localStorage.getItem(username);
+    if(username == undefined){
+        alert("Xin hãy nhập tài khoản và mật khẩu")
+    }
+    else if(username == "Nguyễn Hoàng Vũ" && email == "hoangvu125202@gmail.com" && password == "1111"){
+        alert("Đăng nhập thành công")
+        window.location.href="TrangChu.html"
+    }
+    else{
+        alert("Đăng nhập không thành công")
+    }
+}
+
 //Hiển thị mật khẩu
 function showPassword(){
-    var password = document.getElementById("input-password");
+    var password = document.getElementById("password");
 
     if (password.type === "password"){
         password.type = "text";
