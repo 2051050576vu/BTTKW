e2e6c049941649f53a43ae2854a1fb7aab82b5bf
NguyenHoangVu12 <116646306+NguyenHoangVu12@users.noreply.github.com>
2023-08-20 16:57:38 +0700

    abc

---
+++ BTTHIETKEWEB/javascript.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1;
+    let dd = today.getDate();
+    
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    
+    const formattedToday = dd + '/' + mm + '/' + yyyy;
+
+    var date_txt = document.getElementById('today')
+    date_txt.innerText = formattedToday;
+}
+
